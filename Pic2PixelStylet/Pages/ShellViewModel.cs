7e870f6eb919b26f5c85d4330ac694662b037a33
treattrick <1154174767@qq.com>
2025-01-04 19:21:29 +0800

    完成属性绑定

--- Pic2PixelStylet/Pages/ShellViewModel.cs
+++ Pic2PixelStylet/Pages/ShellViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Pic2PixelStylet.Utils;
 using Stylet;
 
 namespace Pic2PixelStylet.Pages
@@ -27,6 +29,10 @@ namespace Pic2PixelStylet.Pages
         private int _gridHeight;
         private CellInfo[,] _cells;
         private string _testText;
+        private BitmapImage _origianlImage;
+        private string _originalImagePath;
+        private int _pixelCols;
+        private int _pixelRows;
         #endregion
 
         #region properties
@@ -35,6 +41,52 @@ namespace Pic2PixelStylet.Pages
             get => _testText;
             set { _testText = value; }
         }
+
+        public BitmapImage OrigianlImage
+        {
+            get => _origianlImage;
+            set { _origianlImage = value; }
+        }
+
+        public string OriginalImagePath
+        {
+            get => _originalImagePath;
+            set
+            {
+                _originalImagePath = value;
+                OnOriginalImagePathChanged(value);
+            }
+        }
+
+        public bool IsImagedLoaded => _origianlImage != null;
+        public double ScaleFactor { get; set; }
+        public double ImageLeft { get; set; }
+        public double ImageTop { get; set; }
+        public double CropLeft { get; set; }
+        public double CropTop { get; set; }
+        public double CanvasContainerWidth { get; set; }
+        public double CanvasContainerHeight { get; set; }
+        public double CropAreaWidth { get; set; }
+        public double CropAreaHeight { get; set; }
+        public string PixelCols
+        {
+            get => _pixelCols.ToString();
+            set
+            {
+                if (int.TryParse(value, out int result))
+                    _pixelCols = result;
+            }
+        }
+        public string PixelRows
+        {
+            get => _pixelRows.ToString();
+            set
+            {
+                if (int.TryParse(value, out int result))
+                    _pixelRows = result;
+            }
+        }
+        public int Threshhold { get; set; }
         #endregion
 
         #region Constructor
@@ -47,10 +99,22 @@ namespace Pic2PixelStylet.Pages
         }
         #endregion
 
-        #region Methods
-        public void TEST()
+        #region PublicMethods
+
+        #endregion
+
+        #region PrivateMethods
+        private void OnOriginalImagePathChanged(string path)
         {
-            TestText = "TEST";
+            if (path == _originalImagePath)
+                return;
+            _originalImagePath = path;
+            OrigianlImage = ImageProcessor.ConvertTo96DpiBitmapImage(path, out bool success);
+            if (!success)
+            {
+                return;
+            }
+            NotifyOfPropertyChange(nameof(IsImagedLoaded));
         }
         #endregion
     }

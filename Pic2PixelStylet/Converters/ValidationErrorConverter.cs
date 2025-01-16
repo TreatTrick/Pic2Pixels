db61395e367c7a3d26ed957f303bc5bc0cfab916
treattrick <1154174767@qq.com>
2025-01-16 22:56:13 +0800

    修复绑定Validation的Error[0]错误的bug

---
+++ Pic2PixelStylet/Converters/ValidationErrorConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Pic2PixelStylet.Converters
+{
+    public class ValidationErrorsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var errors = value as ReadOnlyObservableCollection<ValidationError>;
+            if (errors != null && errors.Count > 0)
+            {
+                return errors[0].ErrorContent.ToString();
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
